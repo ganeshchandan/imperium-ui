24353a8de09b1026a1508f0317290b8e772d14e5
ganeshchandan <chandan.ganesh92@gmail.com>
2024-06-13 17:51:55 +0530

    Css Changes

--- src/components/footer/menu.tsx
+++ src/components/footer/menu.tsx
@@ -1,5 +1,5 @@
 import Menu from "../../assets/menu.svg";
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setShowMenu } from "../../reducers/filter";
 import { RootState } from "../../store";
@@ -14,38 +14,63 @@ import UserIcon from "../../assets/user-icon.svg";
 interface ICategoryMenu {}
 
 const Footermenu: FC<ICategoryMenu> = () => {
+  const showHideMenuRef = useRef<HTMLDivElement>(null);
   const dispatch = useDispatch();
   const showMenu = useSelector((state: RootState) => state.filter.showMenu);
 
   const handleShowMenu = () => {
+    // ${
+    //   showMenu ? "show-menu" : "hide-menu "
+    // }
     dispatch(setShowMenu(!showMenu));
   };
 
+  useEffect(() => {
+    const showHideMenu = showHideMenuRef.current;
+    if (showHideMenu) {
+      const classList = showHideMenu.classList;
+      if (showMenu) {
+        classList.add("show-menu");
+        classList.remove("hide-menu");
+      } else if (!showMenu && classList.contains("show-menu")) {
+        classList.remove("show-menu");
+        classList.add("hide-menu");
+        setTimeout(() => {
+          classList.remove("hide-menu");
+        }, 500);
+      }
+    }
+  }, [showMenu]);
+
   return (
-    <div className={`footer-menu-container ${showMenu ? "show-menu" : ""}`}>
+    <div ref={showHideMenuRef} className="footer-menu-container">
       <div className="footer-menu">
-        <IconWithName
-          name={SEARCH}
-          imageUrl={SearchIcon}
-          imageAlt={SEARCH}
-          className="footer-category-icon"
-        />
-        <IconWithName
-          name={MENU}
-          imageUrl={MenuDot}
-          imageAlt={MENU}
-          className="footer-category-icon"
-        />
-        <IconWithName
-          name={FILTER}
-          imageUrl={Filter}
-          imageAlt={FILTER}
-          className="footer-category-icon"
-          // onClick={handleFilterClick}
-        />
-        <div className="footer-category-icon">
-          <img src={UserIcon} alt={USER}></img>
+        <div className="footer-category-icons">
+          <div></div>
+          <IconWithName
+            name={SEARCH}
+            imageUrl={SearchIcon}
+            imageAlt={SEARCH}
+            className="footer-category-icon"
+          />
+          <IconWithName
+            name={MENU}
+            imageUrl={MenuDot}
+            imageAlt={MENU}
+            className="footer-category-icon"
+          />
+          <IconWithName
+            name={FILTER}
+            imageUrl={Filter}
+            imageAlt={FILTER}
+            className="footer-category-icon"
+            // onClick={handleFilterClick}
+          />
+          <div className="footer-category-icon">
+            <img src={UserIcon} alt={USER}></img>
+          </div>
         </div>
+        <div className="overlay-div"></div>
         <div className="footer-menu-icon">
           <img
             src={showMenu ? Close : Menu}

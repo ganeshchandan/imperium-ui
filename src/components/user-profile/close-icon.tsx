9c0e2c6f797c3bce0a65aefed16a7426a4390c0e
ganeshchandan <chandan.ganesh92@gmail.com>
2024-07-04 00:32:14 +0530

    Swipe able profile screen

--- src/components/user-profile/close-icon.tsx
+++ src/components/user-profile/close-icon.tsx
@@ -1,29 +1,8 @@
 import { Close } from "@assets";
-import { setShowUserProfile } from "@reducers";
-import { useRef } from "react";
-import { useDispatch } from "react-redux";
 
-const ProfileClose = ({
-  profileRef,
-}: {
-  profileRef: React.RefObject<HTMLDivElement>;
-}) => {
-  const closeRef = useRef<{ closeTimer?: NodeJS.Timeout }>({});
-  const dispatch = useDispatch();
-
-  const handleUserClick = () => {
-    clearTimeout(closeRef.current.closeTimer);
-    if (profileRef.current) {
-      profileRef.current.classList.remove("show-user-profile");
-      profileRef.current.classList.add("hide-user-profile");
-    }
-    closeRef.current.closeTimer = setTimeout(() => {
-      dispatch(setShowUserProfile(false));
-    }, 500);
-  };
-
+const ProfileClose = ({ closeClick }: { closeClick: () => void }) => {
   return (
-    <div className="close-icon" onClick={handleUserClick}>
+    <div className="close-icon" onClick={closeClick}>
       <img src={Close}></img>
     </div>
   );
--- src/components/user-profile/index.tsx
+++ src/components/user-profile/index.tsx
@@ -2,15 +2,39 @@ import UserIconDetails from "./user-icon-details";
 import ProfileClose from "./close-icon";
 import ProfileUserActions from "./actions";
 import { useRef } from "react";
+import { SwipeableHandlers, useSwipeable } from "react-swipeable";
+import { useDispatch } from "react-redux";
+import { setShowUserProfile } from "@reducers";
 
 const UserProfile = () => {
   const profileRef = useRef<HTMLDivElement>(null);
 
+  const closeRef = useRef<{ closeTimer?: NodeJS.Timeout }>({});
+  const dispatch = useDispatch();
+
+  const handleUserClick = () => {
+    clearTimeout(closeRef.current.closeTimer);
+    if (profileRef.current) {
+      profileRef.current.classList.remove("show-user-profile");
+      profileRef.current.classList.add("hide-user-profile");
+    }
+    closeRef.current.closeTimer = setTimeout(() => {
+      dispatch(setShowUserProfile(false));
+    }, 500);
+  };
+
+  const handlers: SwipeableHandlers = useSwipeable({
+    onSwipedLeft: handleUserClick,
+    trackMouse: true,
+  });
+
   return (
     <div className="user-profile show-user-profile" ref={profileRef}>
-      <UserIconDetails />
-      <ProfileUserActions />
-      <ProfileClose profileRef={profileRef} />
+      <div {...handlers} className="swipeable-div">
+        <UserIconDetails />
+        <ProfileUserActions />
+        <ProfileClose closeClick={handleUserClick} />
+      </div>
     </div>
   );
 };
